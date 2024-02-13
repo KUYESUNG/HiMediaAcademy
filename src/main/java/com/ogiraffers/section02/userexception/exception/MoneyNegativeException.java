45210daa275125bce22342414c713c05fddfc410
구예성 <jh8ckr@gmail.com>
2024-02-13 16:28:14 +0900

    chap13

---
+++ src/main/java/com/ogiraffers/section02/userexception/exception/MoneyNegativeException.java
@@ -0,0 +1,8 @@
+package com.ogiraffers.section02.userexception.exception;
+
+public class MoneyNegativeException extends NegativeException {
+
+    public MoneyNegativeException(String message) {
+        super(message);
+    }
+}
