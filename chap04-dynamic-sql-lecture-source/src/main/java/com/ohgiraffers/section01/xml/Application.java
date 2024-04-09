e8644bf44e209b7404f93e6efae1ead411e46eb0
KUYESUNG <157683190+KUYESUNG@users.noreply.github.com>
2024-04-09 13:08:20 +0900

    Add files via upload

--- chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
+++ chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
@@ -149,6 +149,7 @@ public class Application {
             switch (no) {
                 case 1 : menuService.searchMenuByCodeOrSearchAll(inputAllOrOne()); break;
                 case 2 : menuService.searchMenuByNameOrCategory(inputSearchCriteriaMap()); break;
+                case 3 : menuService.modifyMenu(inputChangeInfo()); break;
                 case 9 : return;
             }
         } while (true);
@@ -202,6 +203,25 @@ public class Application {
             criteria.put("nameValue", nameValue);
             criteria.put("categoryValue", categoryValue);
         }
+
+        return criteria;
+    }
+
+    private static Map<String, Object> inputChangeInfo() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("변경할 메뉴 코드를 입력하세요 : ");
+        int code = sc.nextInt();
+        System.out.print("변경할 메뉴 이름을 입력하세요 :");
+        sc.nextLine();
+        String name = sc.nextLine();
+        System.out.print("판매 여부를 결정해주세요(Y/N) : ");
+        String orderableStatus = sc.nextLine();
+
+        Map<String, Object> criteria = new HashMap<>();
+        criteria.put("code", code);
+        criteria.put("name", name);
+        criteria.put("orderableStatus", orderableStatus);
 
         return criteria;
     }
